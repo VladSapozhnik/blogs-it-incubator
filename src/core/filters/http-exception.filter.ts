728db4a5cc137c0d2048e86a563c3b06490b74f1
VladSapozhnik <vladbars2@gmail.com>
2026-01-07 15:43:53 +0200

    validate errors for format

---
+++ src/core/filters/http-exception.filter.ts
@@ -0,0 +1,49 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  BadRequestException,
+  // BadRequestException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+type ValidationErrorResponse = {
+  message: Array<{
+    field: string;
+    message: string;
+  }>;
+};
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+
+    const status = exception.getStatus();
+    const exceptionResponse = exception.getResponse();
+
+    if (exception instanceof BadRequestException) {
+      if (
+        typeof exceptionResponse === 'object' &&
+        exceptionResponse !== null &&
+        'message' in exceptionResponse
+      ) {
+        const errorsMessages = (exceptionResponse as ValidationErrorResponse)
+          .message;
+
+        return response.status(status).json({
+          errorsMessages,
+        });
+      }
+    }
+
+    response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './core/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,8 +9,18 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
+      stopAtFirstError: true,
+      exceptionFactory: (errors) => {
+        const messages = errors.map((err) => ({
+          field: err.property,
+          message: Object.values(err.constraints || {})[0],
+        }));
+        return new BadRequestException(messages);
+      },
     }),
   );
+
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT ?? 3005);
 }
 bootstrap();
