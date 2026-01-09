9c6e9be46637b4a969f3842b7c5f7cbb66e16c8b
VladSapozhnik <vladbars2@gmail.com>
2026-01-09 16:58:29 +0200

    add tests for login and get profile

--- src/core/exceptions/domain-exceptions.ts
+++ src/core/exceptions/domain-exceptions.ts
@@ -1,14 +1,10 @@
-// domain/domain-exception.ts
-export type ErrorMessage = {
-  message: string;
-  field: string;
-};
+import { ErrorMessageType } from '../types/error-message.type';
 
 export class DomainException extends Error {
   readonly status: number;
-  readonly errorsMessages: ErrorMessage[];
+  readonly errorsMessages: ErrorMessageType[];
 
-  constructor(params: { status: number; errorsMessages: ErrorMessage[] }) {
+  constructor(params: { status: number; errorsMessages: ErrorMessageType[] }) {
     super('Domain exception');
     Object.setPrototypeOf(this, new.target.prototype);
     this.status = params.status;
--- test/ helpers/constant.helper.ts
+++ test/ helpers/constant.helper.ts
@@ -11,26 +11,29 @@ export const constantHelper = {
 
   invalidId: '64f0b0c8a1b2c3d4e5f67890',
 
+  invalidToken:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2OTYxMTZhNDRmZmEyY2FlM2ExZTU3MTQiLCJpYXQiOjE3Njc5NzA0NjksImV4cCI6MTc2Nzk3MDg4OX0.3o5qU_XZ0Ru9yxL3LdWNfMEYsh9IptN2mAJ-Mw_hwHU',
+
   users: [
     {
       login: 'usr-1-233',
       email: 'testing@mail.com',
-      password: 'string',
+      password: 'string1',
     },
     {
       login: 'usr-1-223',
       email: 'testing@rail.com',
-      password: 'string',
+      password: 'string2',
     },
     {
       login: 'usr-1-213',
       email: 'testing@cail.com',
-      password: 'string',
+      password: 'string3',
     },
     {
       login: 'usr-1-133',
       email: 'testing@sail.com',
-      password: 'string',
+      password: 'string4',
     },
   ],
 };
