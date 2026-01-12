986441e6680a2c9e248bdb15d17e11264edc8749
VladSapozhnik <vladbars2@gmail.com>
2026-01-12 19:04:36 +0200

    fix

--- src/modules/bloggers-platform/comments/comments.controller.ts
+++ src/modules/bloggers-platform/comments/comments.controller.ts
@@ -36,8 +36,8 @@ export class CommentsController {
     @Param('commentId') commentId: string,
   ) {
     return this.likesExternalService.updateCommentLikeStatus(
-      commentId,
       userId,
+      commentId,
       dto,
     );
   }
