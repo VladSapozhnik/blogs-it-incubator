c56ed8020f7c5553c091fd861767d877d74ba9a8
VladSapozhnik <vladbars2@gmail.com>
2026-01-13 13:25:10 +0200

    fix validate blogs

--- src/modules/bloggers-platform/blogs/dto/create-blog.dto.ts
+++ src/modules/bloggers-platform/blogs/dto/create-blog.dto.ts
@@ -1,10 +1,19 @@
-import { IsString } from 'class-validator';
+import { IsNotEmpty, IsString, Matches, MaxLength } from 'class-validator';
 
 export class CreateBlogDto {
+  @IsNotEmpty()
   @IsString()
+  @MaxLength(15)
   name: string;
+  @IsNotEmpty()
   @IsString()
+  @MaxLength(500)
   description: string;
+  @IsNotEmpty()
   @IsString()
+  @MaxLength(100)
+  @Matches(
+    /^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/,
+  )
   websiteUrl: string;
 }
