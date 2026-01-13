64b14353d799ee6c0a528f01668f402bd8bfbffa
VladSapozhnik <vladbars2@gmail.com>
2026-01-13 13:29:47 +0200

    fix validate posts

--- src/modules/bloggers-platform/posts/dto/create-post-for-blog.dto.ts
+++ src/modules/bloggers-platform/posts/dto/create-post-for-blog.dto.ts
@@ -1,5 +1,16 @@
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+
 export class CreatePostForBlogDto {
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(30)
   title: string;
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(100)
   shortDescription: string;
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(1000)
   content: string;
 }
--- src/modules/bloggers-platform/posts/dto/update-post.dto.ts
+++ src/modules/bloggers-platform/posts/dto/update-post.dto.ts
@@ -1,8 +1,20 @@
 import { Types } from 'mongoose';
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
 export class UpdatePostDto {
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(30)
   title: string;
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(100)
   shortDescription: string;
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(1000)
   content: string;
+  @IsNotEmpty()
+  @IsString()
   blogId: Types.ObjectId;
 }
