ffb949638503381e6a2fa279d514a17f7a162b8f
VladSapozhnik <vladbars2@gmail.com>
2026-01-19 15:49:45 +0200

    add security devices module

---
+++ src/modules/user-accounts/security-devices/entities/security-device.entity.ts
@@ -0,0 +1,28 @@
+import { HydratedDocument, Model, Types } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema({ timestamps: true })
+export class SecurityDevice {
+  @Prop({ required: true })
+  userId: Types.ObjectId;
+  @Prop({ required: true })
+  deviceId: string;
+  @Prop({ required: true })
+  ip: string;
+  @Prop({ required: true })
+  title: string;
+  @Prop({ required: true })
+  lastActiveDate: Date;
+  @Prop({ required: true })
+  expiresAt: Date;
+}
+
+export const SecurityDeviceSchema =
+  SchemaFactory.createForClass(SecurityDevice);
+
+SecurityDeviceSchema.loadClass(SecurityDevice);
+
+export type SecurityDeviceDocument = HydratedDocument<SecurityDevice>;
+
+export type SecurityDeviceModelType = Model<SecurityDeviceDocument> &
+  typeof SecurityDevice;
---
+++ src/modules/user-accounts/security-devices/security-devices.controller.ts
@@ -0,0 +1,43 @@
+import { Controller, Get, Param, Delete, UseGuards } from '@nestjs/common';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { GetSecurityDeviceByUserIdQuery } from './application/queries/get-device-session-by-user-id.query';
+import { RemoveOtherDeviceSessionCommand } from './application/usecase/remove-other-device-session.usecase';
+import { RemoveDeviceSessionCommand } from './application/usecase/remove-device-session.usecase';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { User } from '../auth/decorator/user.decorator';
+import { type JwtPayload } from '../../../core/types/jwt-payload.type';
+import { SecurityDeviceDocument } from './entities/security-device.entity';
+
+@UseGuards(JwtAuthGuard)
+@Controller('security-devices')
+export class SecurityDevicesController {
+  constructor(
+    private readonly queryBus: QueryBus,
+    private readonly commandBus: CommandBus,
+  ) {}
+
+  @Get()
+  getUserDevices(@User() user: JwtPayload) {
+    return this.queryBus.execute<
+      GetSecurityDeviceByUserIdQuery,
+      SecurityDeviceDocument
+    >(new GetSecurityDeviceByUserIdQuery(user.userId));
+  }
+
+  @Delete(':deviceId')
+  removeDeviceSession(
+    @User() user: JwtPayload,
+    @Param('deviceId') deviceId: string,
+  ) {
+    return this.commandBus.execute<RemoveDeviceSessionCommand, void>(
+      new RemoveDeviceSessionCommand(user, deviceId),
+    );
+  }
+
+  @Delete()
+  removeOtherSessions(@User() user: JwtPayload) {
+    return this.commandBus.execute<RemoveOtherDeviceSessionCommand, void>(
+      new RemoveOtherDeviceSessionCommand(user),
+    );
+  }
+}
