4b9e4824361431805cc0e2f84ae36e83fb8d867a
VladSapozhnik <vladbars2@gmail.com>
2026-01-23 16:21:28 +0200

    refactor and fix refresh-token

--- src/core/adapters/cookie.adapter.ts
+++ src/core/adapters/cookie.adapter.ts
@@ -1,12 +1,22 @@
 import { Response } from 'express';
 import { isProdHelper } from '../helpers/is-prod.helper';
+import { UserAccountsConfig } from '../../modules/user-accounts/config/user-accounts.config';
+import ms, { StringValue } from 'ms';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class CookieAdapter {
+  constructor(private readonly userAccountsConfig: UserAccountsConfig) {}
+
   setRefreshCookie(res: Response, token: string) {
+    const refreshTokenMaxAge = ms(
+      this.userAccountsConfig.refreshTokenExpires as StringValue,
+    );
+
     res.cookie('refreshToken', token, {
       httpOnly: true,
       secure: isProdHelper(),
-      maxAge: 20_000,
+      maxAge: refreshTokenMaxAge,
       sameSite: 'strict',
     });
   }
